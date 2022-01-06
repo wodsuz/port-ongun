c5f29b76759f9af6f759677bbd44fa64cbc31a92
Ongun Demirag <ongun.demirag@gmail.com>
2022-01-07 00:13:45 +0300

    projects design

--- components/ProjectsNavbar.tsx
+++ components/ProjectsNavbar.tsx
@@ -1,26 +1,36 @@
 import { FunctionComponent } from "react";
 import { category } from "../type";
 
-export const NavItem: FunctionComponent<{ value: category | "all" }> = ({
-  value,
-}) => {
+export const NavItem: FunctionComponent<{
+  value: category | "all";
+  handlerFilterCategory: Function;
+  active: string;
+}> = ({ value, handlerFilterCategory, active }) => {
+  let className = "capitalize cursor-pointer hover:text-secondary";
+  if (active === value) className += " text-secondary";
   return (
-    <li className="cursor-pointer hover:text-secondary capitalize">{value}</li>
+    <li className={className} onClick={() => handlerFilterCategory(value)}>
+      {value}
+    </li>
   );
 };
 
-const ProjectsNavbar = () => {
+const ProjectsNavbar: FunctionComponent<{
+  handlerFilterCategory: Function;
+  active: string;
+}> = (props) => {
   return (
-    <div className="space-x-3 flex px-3 py-2 overflow-x-auto list-none">
-      <NavItem value="all" />
-      <NavItem value="React" />
-      <NavItem value="Next" />
-      <NavItem value="Node" />
-      <NavItem value="Vanilla(ES6)" />
-      <NavItem value="Python" />
-      <NavItem value="UI" />
-      <NavItem value="SASS" />
-      <NavItem value="Tailwind CSS" />
+    <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
+      {" "}
+      <NavItem value="all" {...props} />
+      <NavItem value="React" {...props} />
+      <NavItem value="Next" {...props} />
+      <NavItem value="Node" {...props} />
+      <NavItem value="Vanilla(ES6)" {...props} />
+      <NavItem value="Python" {...props} />
+      <NavItem value="UI" {...props} />
+      <NavItem value="SASS" {...props} />
+      <NavItem value="Tailwind CSS" {...props} />
     </div>
   );
 };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -22,7 +22,10 @@ const Projects = () => {
   };
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
-      <ProjectsNavbar />
+      <ProjectsNavbar
+        handlerFilterCategory={handlerfilterCategory}
+        active={active}
+      />
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
           <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200">
