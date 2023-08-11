5cf75a98a00c4e203e5746f274c1973efa248ec8
Ongun Demirag <ongun.demirag@gmail.com>
2023-08-12 00:58:46 +0300

    added google analytics

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/stat/:region",
+        destination: "https://:region.google-analytics.com/g/collect",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import { ThemeProvider } from "next-themes";
 import { AnimatePresence } from "framer-motion";
-import { Router } from "next/router";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
+      <GoogleAnalytics
+        gtagUrl="/js/ga-stat.js"
+        strategy="lazyOnload"
+        trackPageViews
+      />
       <div className="grid grid-cols-12 gap-6 px-5 my-8 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
         {/* // do this div style later (after putting the content) */}
         <div className="h-full col-span-12 p-4 text-base text-center bg-white lg:col-span-3 rounded-2xl dark:bg-dark-500 shadow-custom-light dark:shadow-custom-dark">
