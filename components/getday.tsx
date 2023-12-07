d74a6cdb88db920522492071bf0f0792ce963019
wodsuz <ongun.demirag@gmail.com>
2023-12-07 02:59:13 +0100

    added favicon and seo tags

--- components/getday.tsx
+++ components/getday.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Favicon from "./Favicon";
 
 export const Getday = () => {
   var month = new Date().getMonth() + 1;
@@ -9,6 +10,23 @@ export const Getday = () => {
   return (
     <Head>
       <title>{TITLE}</title>
+      <Favicon />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="keywords"
+        content="Ongun Demirag, software engineer, front end dev, back end dev, full stack developer, personal site, portfolio site, projects and resume"
+      />
+      <meta property="og:title" content="Ongun Demirag, software developer." />
+      <meta name="keywords" content="Ongun Demirag, software developer." />
+
+      <meta
+        name="description"
+        content="Personal website of Ongun Demirag, software developer."
+      />
+      <meta
+        property="og:description"
+        content="Personal website of Ongun Demirag, software developer."
+      />
     </Head>
   );
 };
