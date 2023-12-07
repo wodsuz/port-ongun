6973f0c1a6ce3aa1d4f34ec9df969ff8aa6b227b
wodsuz <ongun.demirag@gmail.com>
2023-12-07 02:36:17 +0100

    cleaned the code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
 import { motion } from "framer-motion";
-import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
-  GetStaticProps,
-  GetStaticPropsContext,
-  NextPage,
-} from "next";
+import { NextPage } from "next";
 import { fadeInUp, stagger, routerAnimation } from "../animations";
 import ServiceCard from "../components/ServiceCard";
 import { services } from "../data";
@@ -58,33 +52,5 @@ const index: NextPage = () => {
     </motion.div>
   );
 };
-
-//!called every time  the page refreshed
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  /*
-  const res = await fetch(`${process.env.VERCEL_URL}/api/services`);
-  const data = await res.json();
-  console.log(data);
-  */
-  return { props: { endpoint: process.env.VERCEL_URL } };
-};
-
-//!called only during the build of the project
-//? make sure the server(localhost:3000)[this will receive the request during build] is running on a terminal during the build
-//? also need to change the localhost during the deployment | see the todo
-// https://aude53.medium.com/set-environment-variables-with-next-js-and-vercel-e544c0460a48
-
-// export const getStaticProps: GetStaticProps = async (
-//    context: GetStaticPropsContext
-// ) => {
-//    // console.log(context);
-
-//    const res = await fetch('http://localhost:3000/api/services')
-//    const { services } = await res.json()
-//    console.log({ services })
-//    return { props: { services: services } }
-// }
 
 export default index;
